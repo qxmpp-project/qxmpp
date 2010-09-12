92c06871e68d028baa2fb05d5464bb1d6847173c
Manjeet Dahiya <manjeetdahiya@gmail.com>
2010-09-12 09:53:22 +0000

    remove unnecessary commented

--- examples/GuiClient/vCardManager.cpp
+++ examples/GuiClient/vCardManager.cpp
@@ -50,33 +50,6 @@ void vCardManager::vCardReceived(const QXmppVCardIq& vcard)
 
     m_mapBareJidVcard[from] = vcard;
 
-//    if(m_mapBareJidVCard[from].imageHash == getImageHash(vcard.photo()))
-//    {
-//        emit vCardReadyToUse(from);
-//        return;
-//    }
-
-//    if(!m_mapBareJidVCard[from].imageHash.isEmpty())
-//    {
-//        QString fileName = getSettingsDir() + m_mapBareJidVCard[from].imageHash;
-//        QFile file1(fileName + "_original.png");
-//        QFile file2(fileName + "_scaled.png");
-//        file1.remove();
-//        file2.remove();
-//    }
-
-//    m_mapBareJidVCard[from].imageHash = getImageHash(vcard.photo());
-
-//    QImage image = getImageFromByteArray(vcard.photo());
-//    m_mapBareJidVCard[from].imageOriginal = image;
-
-//    if(!image.isNull())
-//        m_mapBareJidVCard[from].image = image.scaledToWidth(32);
-
-//    QString fileName = getSettingsDir() + m_mapBareJidVCard[from].imageHash;
-//    m_mapBareJidVCard[from].imageOriginal.save(fileName + "_original.png", "PNG");
-//    m_mapBareJidVCard[from].image.save(fileName + "_scaled.png", "PNG");
-
     saveToCache(from);
 
     emit vCardReadyToUse(from);
@@ -101,21 +74,6 @@ QXmppVCardIq& vCardManager::getVCard(const QString& bareJid)
 
 void vCardManager::saveToCache(const QString& bareJid)
 {
-//    QString fileBareJidImageMap = getSettingsDir() + m_client->configuration().jidBare() + "_jidimage";
-//    QFile file(fileBareJidImageMap);
-
-//    if(!file.open(QIODevice::ReadWrite))
-//        return;
-
-//    QTextStream out(&file);
-//    QMap<QString, vCardManager::vCard>::const_iterator i = m_mapBareJidVCard.constBegin();
-//    while(i != m_mapBareJidVCard.constEnd())
-//    {
-//        out << i.key() << "\n" << i.value().imageHash << "\n";
-//        ++i;
-//    }
-//    file.close();
-
     QDir dir;
     if(!dir.exists(getSettingsDir(m_client->configuration().jidBare())))
         dir.mkpath(getSettingsDir(m_client->configuration().jidBare()));
@@ -136,40 +94,6 @@ void vCardManager::saveToCache(const QString& bareJid)
             file.close();
         }
     }
-
-//    if(!m_mapBareJidVCard.contains(bareJid))
-//        return;
-//    QString fileName = getSettingsDir() + m_mapBareJidVCard[bareJid].imageHash;
-//    m_mapBareJidVCard[bareJid].imageOriginal.save(fileName + "_original.png", "PNG");
-//    m_mapBareJidVCard[bareJid].image.save(fileName + "_scaled.png", "PNG");
-//
-//    QString fileBareJidImageMap = getSettingsDir() + m_client->configuration().jidBare() + "_jidimage.xml";
-//
-//    if(m_domJidImage.documentElement().isNull())
-//    {
-//        QDomElement root = m_domJidImage.createElement("jidimage");
-//        m_domJidImage.appendChild(root);
-//    }
-//
-//    QFile file(fileBareJidImageMap);
-//    if(!file.open(QIODevice::ReadWrite))
-//        return;
-//
-//    if(m_domJidImage.documentElement().firstChildElement(bareJid).isNull())
-//    {
-//        QDomElement tag = m_domJidImage.createElement(bareJid);
-//        m_domJidImage.documentElement().appendChild(tag);
-//        QDomText t = m_domJidImage.createTextNode(m_mapBareJidVCard[bareJid].imageHash);
-//        tag.appendChild(t);
-//    }
-//    else
-//    {
-//        m_domJidImage.documentElement().firstChildElement(bareJid).firstChild().setNodeValue(m_mapBareJidVCard[bareJid].imageHash);
-//    }
-//
-//    QTextStream out(&file);
-//    m_domJidImage.save(out, 4);
-//    file.close();
 }
 
 void vCardManager::loadAllFromCache()
@@ -196,54 +120,6 @@ void vCardManager::loadAllFromCache()
             }
         }
     }
-
-//    QString fileBareJidImageMap = getSettingsDir() + m_client->configuration().jidBare() + "_jidimage";
-//    QFile file(fileBareJidImageMap);
-//    if(!file.open(QIODevice::ReadOnly))
-//        return;
-
-//    QString bareJid, imageHash;
-//    while(!file.atEnd())
-//    {
-//        bareJid = file.readLine();
-//        bareJid = bareJid.trimmed();
-//        imageHash = file.readLine();
-//        imageHash = imageHash.trimmed();
-
-//        if(m_mapBareJidVcard.contains(bareJid))
-//            m_mapBareJidVCard[bareJid].fullName = m_mapBareJidVcard[bareJid].fullName();
-
-//        m_mapBareJidVCard[bareJid].imageHash = imageHash;
-//        m_mapBareJidVCard[bareJid].image.load(
-//                getSettingsDir() + m_mapBareJidVCard[bareJid].imageHash + "_scaled.png");
-//        m_mapBareJidVCard[bareJid].imageOriginal.load(
-//                getSettingsDir() + m_mapBareJidVCard[bareJid].imageHash + "_original.png");
-//        QCoreApplication::processEvents(QEventLoop::ExcludeUserInputEvents);
-//    }
-//    file.close();
-
-
-//    QString fileBareJidImageMap = getSettingsDir() + m_client->configuration().jidBare() + "_jidimage.xml";
-//    QDomDocument doc(m_client->configuration().jidBare() + "_jidimage");
-//    QFile file(fileBareJidImageMap);
-//
-//    if(!file.open(QIODevice::ReadOnly))
-//        return;
-//
-//    QString error;
-//    if(m_domJidImage.setContent(&file, false, &error))
-//    {
-//        QDomElement nodeRecv = doc.documentElement().firstChildElement();
-//        while(!nodeRecv.isNull())
-//        {
-//            m_mapBareJidVCard[nodeRecv.tagName()].imageHash = nodeRecv.text();
-//            bool check = m_mapBareJidVCard[nodeRecv.tagName()].image.load(
-//                    getSettingsDir() + m_mapBareJidVCard[nodeRecv.tagName()].imageHash + "_scaled.png");
-//            m_mapBareJidVCard[nodeRecv.tagName()].imageOriginal.load(
-//                    getSettingsDir() + m_mapBareJidVCard[nodeRecv.tagName()].imageHash + "_original.png");
-//            nodeRecv = nodeRecv.nextSiblingElement();
-//        }
-//    }
 }
 
 QString vCardManager::getSelfFullName()
