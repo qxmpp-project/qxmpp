122e4b0dbc3b58ffad94d15e401e5a0a37ec8bab
Manjeet Dahiya <manjeetdahiya@gmail.com>
2010-09-21 00:29:25 +0000

    destructor should be virtual

--- src/QXmppClientExtension.h
+++ src/QXmppClientExtension.h
@@ -43,7 +43,7 @@ class QXmppClientExtension : public QObject
 
 public:
     QXmppClientExtension();
-    ~QXmppClientExtension();
+    virtual ~QXmppClientExtension();
 
     virtual QStringList discoveryFeatures() const;
     virtual bool handleStanza(QXmppStream *stream, const QDomElement &stanza) = 0;
