84b6b23fd6335802c42e67d3cc81cfbf01c80c3a
Manjeet Dahiya <manjeetdahiya@gmail.com>
2011-03-05 04:09:56 +0000

    fix tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -135,7 +135,7 @@ void TestUtils::testMime()
 
 void TestUtils::testLibVersion()
 {
-    QCOMPARE(QXmppVersion(), QString("0.2.92"));
+    QCOMPARE(QXmppVersion(), QString("0.3.0"));
 }
 
 void TestUtils::testTimezoneOffset()
