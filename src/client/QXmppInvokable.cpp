f260085ef199428e9287a3f36387d697709af4ef
Jeremy Lainé <jeremy.laine@m4x.org>
2012-06-06 06:53:07 +0200

    Fix build with Qt5 (Fixes issue #142)

--- src/client/QXmppInvokable.cpp
+++ src/client/QXmppInvokable.cpp
@@ -110,7 +110,11 @@ void QXmppInvokable::buildMethodHash( )
     int methodCount = metaObject()->methodCount ();
     for( int idx = 0; idx < methodCount; ++idx)
     {
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
+        QByteArray signature = metaObject()->method(idx).methodSignature();
+#else
         QByteArray signature = metaObject()->method(idx).signature();
+#endif
         m_methodHash[signature.left(signature.indexOf('('))] = idx;
 //         qDebug() << metaObject()->method(idx).parameterTypes();
     }
@@ -124,7 +128,11 @@ QStringList QXmppInvokable::interfaces( ) const
     {
         if( metaObject()->method(idx).methodType() == QMetaMethod::Slot )
         {
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
+            QByteArray signature = metaObject()->method(idx).methodSignature();
+#else
             QByteArray signature = metaObject()->method(idx).signature();
+#endif
             results << signature.left(signature.indexOf('('));
         }
     }
