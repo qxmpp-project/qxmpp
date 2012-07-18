bbde6664a259c81c0c7ace748d083c8bc3364d5e
Jeremy Lainé <jeremy.laine@m4x.org>
2012-07-18 11:41:29 +0200

    re-arrange namespace constants

--- src/base/QXmppConstants.cpp
+++ src/base/QXmppConstants.cpp
@@ -27,71 +27,88 @@
 const char* ns_stream = "http://etherx.jabber.org/streams";
 const char* ns_client = "jabber:client";
 const char* ns_server = "jabber:server";
-const char* ns_server_dialback = "jabber:server:dialback";
 const char* ns_roster = "jabber:iq:roster";
 const char* ns_tls = "urn:ietf:params:xml:ns:xmpp-tls";
 const char* ns_sasl = "urn:ietf:params:xml:ns:xmpp-sasl";
 const char* ns_bind = "urn:ietf:params:xml:ns:xmpp-bind";
 const char* ns_session = "urn:ietf:params:xml:ns:xmpp-session";
 const char* ns_stanza = "urn:ietf:params:xml:ns:xmpp-stanzas";
-const char* ns_auth = "jabber:iq:auth";
-const char* ns_authFeature = "http://jabber.org/features/iq-auth";
-const char* ns_capabilities = "http://jabber.org/protocol/caps";
-const char* ns_compress = "http://jabber.org/protocol/compress";
-const char* ns_compressFeature = "http://jabber.org/features/compress";
-const char *ns_legacy_delayed_delivery = "jabber:x:delay";
 // XEP-0009: Jabber-RPC
 const char* ns_rpc = "jabber:iq:rpc";
 // XEP-0020: Feature Negotiation
-const char *ns_feature_negotiation = "http://jabber.org/protocol/feature-neg";
+const char* ns_feature_negotiation = "http://jabber.org/protocol/feature-neg";
 // XEP-0030: Service Discovery
 const char* ns_disco_info = "http://jabber.org/protocol/disco#info";
 const char* ns_disco_items = "http://jabber.org/protocol/disco#items";
+// XEP-0033: Extended Stanza Addressing
+const char* ns_extended_addressing = "http://jabber.org/protocol/address";
 // XEP-0045: Multi-User Chat
-const char *ns_muc = "http://jabber.org/protocol/muc";
-const char *ns_muc_admin = "http://jabber.org/protocol/muc#admin";
-const char *ns_muc_owner = "http://jabber.org/protocol/muc#owner";
-const char *ns_muc_user = "http://jabber.org/protocol/muc#user";
+const char* ns_muc = "http://jabber.org/protocol/muc";
+const char* ns_muc_admin = "http://jabber.org/protocol/muc#admin";
+const char* ns_muc_owner = "http://jabber.org/protocol/muc#owner";
+const char* ns_muc_user = "http://jabber.org/protocol/muc#user";
 // XEP-0047: In-Band Bytestreams
 const char* ns_ibb = "http://jabber.org/protocol/ibb";
+// XEP-0049: Private XML Storage
+const char* ns_private = "jabber:iq:private";
 // XEP-0054: vcard-temp
 const char* ns_vcard = "vcard-temp";
 // XEP-0059: Result Set Management
-const char *ns_rsm = "http://jabber.org/protocol/rsm";
+const char* ns_rsm = "http://jabber.org/protocol/rsm";
 // XEP-0065: SOCKS5 Bytestreams
-const char *ns_bytestreams = "http://jabber.org/protocol/bytestreams";
+const char* ns_bytestreams = "http://jabber.org/protocol/bytestreams";
 // XEP-0077: In-Band Registration
-const char *ns_register = "jabber:iq:register";
+const char* ns_register = "jabber:iq:register";
+// XEP-0078: Non-SASL Authentication
+const char* ns_auth = "jabber:iq:auth";
+const char* ns_authFeature = "http://jabber.org/features/iq-auth";
 // XEP-0085: Chat State Notifications
-const char *ns_chat_states = "http://jabber.org/protocol/chatstates";
+const char* ns_chat_states = "http://jabber.org/protocol/chatstates";
+// XEP-0091: Legacy Delayed Delivery
+const char* ns_legacy_delayed_delivery = "jabber:x:delay";
 // XEP-0092: Software Version
-const char *ns_version = "jabber:iq:version";
-const char *ns_data = "jabber:x:data";
+const char* ns_version = "jabber:iq:version";
+const char* ns_data = "jabber:x:data";
 // XEP-0095: Stream Initiation
-const char *ns_stream_initiation = "http://jabber.org/protocol/si";
-const char *ns_stream_initiation_file_transfer = "http://jabber.org/protocol/si/profile/file-transfer";
+const char* ns_stream_initiation = "http://jabber.org/protocol/si";
+const char* ns_stream_initiation_file_transfer = "http://jabber.org/protocol/si/profile/file-transfer";
+// XEP-0108: User Activity
+const char* ns_activity = "http://jabber.org/protocol/activity";
+// XEP-0115: Entity Capabilities
+const char* ns_capabilities = "http://jabber.org/protocol/caps";
 // XEP-0136: Message Archiving
-const char *ns_archive = "urn:xmpp:archive";
+const char* ns_archive = "urn:xmpp:archive";
+// XEP-0138: Stream Compression
+const char* ns_compress = "http://jabber.org/protocol/compress";
+const char* ns_compressFeature = "http://jabber.org/features/compress";
+// XEP-0145: Annotations
+const char* ns_rosternotes = "storage:rosternotes";
 // XEP-0153: vCard-Based Avatars
 const char* ns_vcard_update = "vcard-temp:x:update";
+// XEP-0158: CAPTCHA Forms
+const char* ns_captcha = "urn:xmpp:captcha";
 // XEP-0166: Jingle
-const char *ns_jingle = "urn:xmpp:jingle:1";
-const char *ns_jingle_raw_udp = "urn:xmpp:jingle:transports:raw-udp:1";
+const char* ns_jingle = "urn:xmpp:jingle:1";
+const char* ns_jingle_raw_udp = "urn:xmpp:jingle:transports:raw-udp:1";
 const char* ns_jingle_ice_udp = "urn:xmpp:jingle:transports:ice-udp:1";
-const char *ns_jingle_rtp = "urn:xmpp:jingle:apps:rtp:1";
-const char *ns_jingle_rtp_audio = "urn:xmpp:jingle:apps:rtp:audio";
-const char *ns_jingle_rtp_video = "urn:xmpp:jingle:apps:rtp:video";
+const char* ns_jingle_rtp = "urn:xmpp:jingle:apps:rtp:1";
+const char* ns_jingle_rtp_audio = "urn:xmpp:jingle:apps:rtp:audio";
+const char* ns_jingle_rtp_video = "urn:xmpp:jingle:apps:rtp:video";
 // XEP-0184: Message Receipts
-const char *ns_message_receipts = "urn:xmpp:receipts";
+const char* ns_message_receipts = "urn:xmpp:receipts";
 // XEP-0199: XMPP Ping
-const char *ns_ping = "urn:xmpp:ping";
+const char* ns_ping = "urn:xmpp:ping";
 // XEP-0202: Entity Time
-const char *ns_entity_time = "urn:xmpp:time";
+const char* ns_entity_time = "urn:xmpp:time";
 // XEP-0203: Delayed Delivery
-const char *ns_delayed_delivery = "urn:xmpp:delay";
+const char* ns_delayed_delivery = "urn:xmpp:delay";
+// XEP-0220: Server Dialback
+const char* ns_server_dialback = "jabber:server:dialback";
 // XEP-0221: Data Forms Media Element
-const char *ns_media_element = "urn:xmpp:media-element";
+const char* ns_media_element = "urn:xmpp:media-element";
 // XEP-0224: Attention
-const char *ns_attention = "urn:xmpp:attention:0";
+const char* ns_attention = "urn:xmpp:attention:0";
+// XEP-0231: Bits of Binary
+const char* ns_bob = "urn:xmpp:bob";
 // XEP-0249: Direct MUC Invitations
-const char *ns_conference = "jabber:x:conference";
+const char* ns_conference = "jabber:x:conference";
--- src/base/QXmppConstants.h
+++ src/base/QXmppConstants.h
@@ -28,73 +28,90 @@
 extern const char* ns_stream;
 extern const char* ns_client;
 extern const char* ns_server;
-extern const char* ns_server_dialback;
 extern const char* ns_roster;
 extern const char* ns_tls;
 extern const char* ns_sasl;
 extern const char* ns_bind;
 extern const char* ns_session;
 extern const char* ns_stanza;
-extern const char* ns_auth;
-extern const char* ns_authFeature;
-extern const char* ns_capabilities;
-extern const char* ns_compress;
-extern const char* ns_compressFeature;
-extern const char *ns_legacy_delayed_delivery;
 // XEP-0009: Jabber-RPC
 extern const char* ns_rpc;
 // XEP-0020: Feature Negotiation
-extern const char *ns_feature_negotiation;
+extern const char* ns_feature_negotiation;
 // XEP-0030: Service Discovery
 extern const char* ns_disco_info;
 extern const char* ns_disco_items;
+// XEP-0033: Extended Stanza Addressing
+extern const char* ns_extended_addressing;
 // XEP-0045: Multi-User Chat
-extern const char *ns_muc;
-extern const char *ns_muc_admin;
-extern const char *ns_muc_owner;
-extern const char *ns_muc_user;
+extern const char* ns_muc;
+extern const char* ns_muc_admin;
+extern const char* ns_muc_owner;
+extern const char* ns_muc_user;
 // XEP-0047: In-Band Bytestreams
 extern const char* ns_ibb;
+// XEP-0049: Private XML Storage
+extern const char* ns_private;
 // XEP-0054: vcard-temp
 extern const char* ns_vcard;
 // XEP-0059: Result Set Management
-extern const char *ns_rsm;
+extern const char* ns_rsm;
 // XEP-0065: SOCKS5 Bytestreams
-extern const char *ns_bytestreams;
+extern const char* ns_bytestreams;
 // XEP-0077: In-Band Registration
-extern const char *ns_register;
+extern const char* ns_register;
+// XEP-0078: Non-SASL Authentication
+extern const char* ns_auth;
+extern const char* ns_authFeature;
 // XEP-0085: Chat State Notifications
-extern const char *ns_chat_states;
+extern const char* ns_chat_states;
+// XEP-0091: Legacy Delayed Delivery
+extern const char* ns_legacy_delayed_delivery;
 // XEP-0092: Software Version
-extern const char *ns_version;
-extern const char *ns_data;
+extern const char* ns_version;
+extern const char* ns_data;
 // XEP-0095: Stream Initiation
-extern const char *ns_stream_initiation;
-extern const char *ns_stream_initiation_file_transfer;
+extern const char* ns_stream_initiation;
+extern const char* ns_stream_initiation_file_transfer;
+// XEP-0108: User Activity
+extern const char* ns_activity;
+// XEP-0115: Entity Capabilities
+extern const char* ns_capabilities;
 // XEP-0136: Message Archiving
-extern const char *ns_archive;
+extern const char* ns_archive;
+// XEP-0138: Stream Compression
+extern const char* ns_compress;
+extern const char* ns_compressFeature;
+// XEP-0145: Annotations
+extern const char* ns_rosternotes;
 // XEP-0153: vCard-Based Avatars
 extern const char* ns_vcard_update;
+// XEP-0158: CAPTCHA Forms
+extern const char* ns_captcha;
 // XEP-0166: Jingle
-extern const char *ns_jingle;
+extern const char* ns_jingle;
 extern const char* ns_jingle_ice_udp;
 extern const char* ns_jingle_raw_udp;
-extern const char *ns_jingle_rtp;
-extern const char *ns_jingle_rtp_audio;
-extern const char *ns_jingle_rtp_video;
+extern const char* ns_jingle_rtp;
+extern const char* ns_jingle_rtp_audio;
+extern const char* ns_jingle_rtp_video;
 // XEP-0184: Message Receipts
-extern const char *ns_message_receipts;
+extern const char* ns_message_receipts;
 // XEP-0199: XMPP Ping
 extern const char* ns_ping;
 // XEP-0202: Entity Time
-extern const char *ns_entity_time;
+extern const char* ns_entity_time;
 // XEP-0203: Delayed Delivery
-extern const char *ns_delayed_delivery;
+extern const char* ns_delayed_delivery;
+// XEP-0220: Server Dialback
+extern const char* ns_server_dialback;
 // XEP-0221: Data Forms Media Element
-extern const char *ns_media_element;
+extern const char* ns_media_element;
 // XEP-0224: Attention
-extern const char *ns_attention;
+extern const char* ns_attention;
+// XEP-0231: Bits of Binary
+extern const char* ns_bob;
 // XEP-0249: Direct MUC Invitations
-extern const char *ns_conference;
+extern const char* ns_conference;
 
 #endif // QXMPPCONSTANTS_H
