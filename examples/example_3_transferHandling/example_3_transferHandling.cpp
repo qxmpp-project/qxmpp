237e65ba4bbc008d020bb212c7368eec1ae2372e
Jeremy Lainé <jeremy.laine@m4x.org>
2012-09-17 20:55:51 +0200

    embed file to send

--- examples/example_3_transferHandling/example_3_transferHandling.cpp
+++ examples/example_3_transferHandling/example_3_transferHandling.cpp
@@ -119,7 +119,7 @@ void xmppClient::slotPresenceReceived(const QXmppPresence &presence)
         return;
 
     // send the file and connect to the job's signals
-    QXmppTransferJob *job = transferManager->sendFile(presence.from(), "xmppClient.cpp");
+    QXmppTransferJob *job = transferManager->sendFile(presence.from(), ":/example_3_transferHandling.cpp");
 
     check = connect(job, SIGNAL(error(QXmppTransferJob::Error)),
                     this, SLOT(slotError(QXmppTransferJob::Error)));
