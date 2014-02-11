89f1d7c3321134895f1f840f435fc107854962c9
Sam Truscott <sam@wumpus.co.uk>
2014-02-11 14:04:00 +0000

    Fix whitespace in QXmppMessage.cpp

--- src/base/QXmppMessage.cpp
+++ src/base/QXmppMessage.cpp
@@ -116,7 +116,6 @@ public:
     // XEP-0308: Last Message Correction
     bool replace;
     QString replaceId;
-
 };
 
 /// Constructs a QXmppMessage.
@@ -494,7 +493,6 @@ void QXmppMessage::setReplace(const QString& replaceId)
     d->replace   = true;
     d->replaceId = replaceId;
 }
-
 
 /// \cond
 void QXmppMessage::parse(const QDomElement &element)
