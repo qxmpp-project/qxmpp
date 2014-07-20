4e8734c75e38be6af4d3882e4858d716317f16cd
Jeremy Lainé <jeremy.laine@m4x.org>
2014-07-20 20:44:37 +0200

    actually test with clang

--- tests/travis/build-and-test.sh
+++ tests/travis/build-and-test.sh
@@ -2,6 +2,16 @@
 set -e
 
 BUILD_FLAGS=""
+
+if [ -n "$CC" ]; then
+    BUILD_FLAGS="$BUILD_FLAGS QMAKE_CC=$CC"
+fi
+
+
+if [ -n "$CXX" ]; then
+    BUILD_FLAGS="$BUILD_FLAGS QMAKE_CXX=$CXX"
+fi
+
 case "$CONFIG" in
 full*)
     BUILD_FLAGS="$BUILD_FLAGS QXMPP_USE_SPEEX=1 QXMPP_USE_THEORA=1"
