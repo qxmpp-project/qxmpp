f6149cef3b4f87689a481efad47a9ddef4995889
Jeremy Lainé <jeremy.laine@m4x.org>
2014-07-20 18:43:25 +0200

    remove compiler flags from tests

--- tests/travis/build-and-test.sh
+++ tests/travis/build-and-test.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -e
 
-BUILD_FLAGS="QMAKE_CXXFLAGS += '-O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -D_FORTIFY_SOURCE=2'" \
-            "QMAKE_LFLAGS += '-Wl,-z,relro -Wl,--as-needed'"
-
+BUILD_FLAGS=""
 case "$CONFIG" in
 full*)
     BUILD_FLAGS="$BUILD_FLAGS QXMPP_USE_SPEEX=1 QXMPP_USE_THEORA=1"
