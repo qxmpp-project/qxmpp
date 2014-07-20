e9f89878d3b7af0fd48d2865a47e7b8039aec128
Jeremy Lainé <jeremy.laine@m4x.org>
2014-07-20 18:22:28 +0200

    test static builds

--- tests/travis/build-and-test.sh
+++ tests/travis/build-and-test.sh
@@ -3,12 +3,19 @@
 BUILD_FLAGS="QMAKE_CXXFLAGS += '-O2 -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -D_FORTIFY_SOURCE=2'" \
             "QMAKE_LFLAGS += '-Wl,-z,relro -Wl,--as-needed'"
 
-if [ "$CONFIG" = "full" ]; then
-    qmake $BUILD_FLAGS QXMPP_USE_SPEEX=1 QXMPP_USE_THEORA=1
-else
-    qmake $BUILD_FLAGS QXMPP_USE_SPEEX="" QXMPP_USE_THEORA=""
-fi
+case "$CONFIG" in
+full*)
+    BUILD_FLAGS="$BUILD_FLAGS QXMPP_USE_SPEEX=1 QXMPP_USE_THEORA=1"
+    ;;
+esac
 
+case "$CONFIG" in
+*static)
+    BUILD_FLAGS="$BUILD_FLAGS QXMPP_LIBRARY_TYPE=staticlib"
+    ;;
+esac
+
+qmake $BUILD_FLAGS
 make VERBOSE=1
 
 tests/run.py
