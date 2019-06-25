c5c230f80cfcd9cde3f993d6d5c6c3e6fb33e82f
Linus Jahn <lnj@kaidan.im>
2019-06-25 12:44:42 +0200

    Add tests for setters of QXmppStreamFeatures

--- tests/qxmppstreamfeatures/tst_qxmppstreamfeatures.cpp
+++ tests/qxmppstreamfeatures/tst_qxmppstreamfeatures.cpp
@@ -32,6 +32,7 @@ private slots:
     void testEmpty();
     void testRequired();
     void testFull();
+    void testSetters();
 };
 
 void tst_QXmppStreamFeatures::testEmpty()
@@ -90,6 +91,30 @@ void tst_QXmppStreamFeatures::testFull()
     QCOMPARE(features.authMechanisms(), QStringList() << "PLAIN");
     QCOMPARE(features.compressionMethods(), QStringList() << "zlib");
     serializePacket(features, xml);
+}
+
+void tst_QXmppStreamFeatures::testSetters()
+{
+    QXmppStreamFeatures features;
+    features.setBindMode(QXmppStreamFeatures::Enabled);
+    QCOMPARE(features.bindMode(), QXmppStreamFeatures::Enabled);
+    features.setSessionMode(QXmppStreamFeatures::Enabled);
+    QCOMPARE(features.sessionMode(), QXmppStreamFeatures::Enabled);
+    features.setNonSaslAuthMode(QXmppStreamFeatures::Enabled);
+    QCOMPARE(features.nonSaslAuthMode(), QXmppStreamFeatures::Enabled);
+    features.setTlsMode(QXmppStreamFeatures::Enabled);
+    QCOMPARE(features.tlsMode(), QXmppStreamFeatures::Enabled);
+    features.setClientStateIndicationMode(QXmppStreamFeatures::Enabled);
+    QCOMPARE(features.clientStateIndicationMode(), QXmppStreamFeatures::Enabled);
+    features.setClientStateIndicationMode(QXmppStreamFeatures::Enabled);
+    QCOMPARE(features.clientStateIndicationMode(), QXmppStreamFeatures::Enabled);
+    features.setRegisterMode(QXmppStreamFeatures::Enabled);
+    QCOMPARE(features.registerMode(), QXmppStreamFeatures::Enabled);
+
+    features.setAuthMechanisms(QStringList() << "custom-mechanism");
+    QCOMPARE(features.authMechanisms(), QStringList() << "custom-mechanism");
+    features.setCompressionMethods(QStringList() << "compression-methods");
+    QCOMPARE(features.compressionMethods(), QStringList() << "compression-methods");
 }
 
 QTEST_MAIN(tst_QXmppStreamFeatures)
