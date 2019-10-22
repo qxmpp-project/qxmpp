8516a8d24ec232af9ccf5450f104cd4cc9519bd8
Linus Jahn <lnj@kaidan.im>
2019-10-22 21:04:15 +0200

    Add minimal test for QXmppClient

---
+++ tests/qxmppclient/tst_qxmppclient.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2008-2019 The QXmpp developers
+ *
+ * Authors:
+ *  Linus Jahn
+ *
+ * Source:
+ *  https://github.com/qxmpp-project/qxmpp
+ *
+ * This file is a part of QXmpp library.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include <QObject>
+
+#include "QXmppClient.h"
+#include "QXmppLogger.h"
+#include "QXmppMessage.h"
+#include "util.h"
+
+class tst_QXmppClient : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void initTestCase();
+
+    void testSendMessage();
+    void handleMessageSent(QXmppLogger::MessageType type, const QString &text) const;
+
+private:
+    QXmppClient *client;
+};
+
+void tst_QXmppClient::handleMessageSent(QXmppLogger::MessageType type, const QString& text) const
+{
+    QCOMPARE(type, QXmppLogger::MessageType::SentMessage);
+
+    QXmppMessage msg;
+    parsePacket(msg, text.toUtf8());
+
+    QCOMPARE(msg.from(), QString());
+    QCOMPARE(msg.to(), QStringLiteral("support@qxmpp.org"));
+    QCOMPARE(msg.body(), QStringLiteral("implement XEP-* plz"));
+}
+
+void tst_QXmppClient::initTestCase()
+{
+    client = new QXmppClient(this);
+}
+
+void tst_QXmppClient::testSendMessage()
+{
+    QXmppLogger logger;
+    logger.setLoggingType(QXmppLogger::SignalLogging);
+    client->setLogger(&logger);
+
+    connect(&logger, &QXmppLogger::message, this, &tst_QXmppClient::handleMessageSent);
+
+    client->sendMessage(
+        QStringLiteral("support@qxmpp.org"),
+        QStringLiteral("implement XEP-* plz")
+    );
+
+    // see handleMessageSent()
+
+    client->setLogger(nullptr);
+}
+
+QTEST_MAIN(tst_QXmppClient)
+#include "tst_qxmppclient.moc"
