86ec9cce367a96ba3dda1926c04500b381f0d701
Linus Jahn <lnj@kaidan.im>
2019-12-14 22:44:20 +0100

    Add QXmppStartTlsPacket to replace hard-coded XML in TLS code

---
+++ src/base/QXmppStartTlsPacket.cpp
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2008-2019 The QXmpp developers
+ *
+ * Authors:
+ *  Linus Jahn
+ *
+ * Source:
+ *  https://github.com/qxmpp-project/qxmpp
+ *
+ * This file is a part of QXmpp library.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include "QXmppStartTlsPacket.h"
+
+#include "QXmppConstants_p.h"
+
+#include <QDomElement>
+#include <QXmlStreamWriter>
+
+const static QStringList STARTTLS_TYPES = {
+    QStringLiteral("starttls"),
+    QStringLiteral("proceed"),
+    QStringLiteral("failure")
+};
+
+/// Constructs a new QXmppStartTlsPacket
+///
+/// \param type The type of the new QXmppStartTlsPacket.
+
+QXmppStartTlsPacket::QXmppStartTlsPacket(Type type)
+    : m_type(type)
+{
+}
+
+QXmppStartTlsPacket::~QXmppStartTlsPacket() = default;
+
+/// Returns the type of the STARTTLS packet
+
+QXmppStartTlsPacket::Type QXmppStartTlsPacket::type() const
+{
+    return m_type;
+}
+
+/// Sets the type of the STARTTLS packet
+
+void QXmppStartTlsPacket::setType(QXmppStartTlsPacket::Type type)
+{
+    m_type = type;
+}
+
+/// \cond
+void QXmppStartTlsPacket::parse(const QDomElement &element)
+{
+    if (!QXmppStartTlsPacket::isStartTlsPacket(element))
+        return;
+
+    m_type = Type(STARTTLS_TYPES.indexOf(element.tagName()));
+}
+
+void QXmppStartTlsPacket::toXml(QXmlStreamWriter *writer) const
+{
+    writer->writeStartElement(STARTTLS_TYPES.at(int(m_type)));
+    writer->writeAttribute(QStringLiteral("xmlns"), ns_tls);
+    writer->writeEndElement();
+}
+/// \endcond
+
+/// Checks whether the given \p element is a STARTTLS packet according to
+/// <a href="https://xmpp.org/rfcs/rfc6120.html#tls-process-initiate">RFC6120</a>.
+///
+/// \param element The element that should be checked for being a STARTTLS packet.
+///
+/// \returns True, if the element is a STARTTLS packet.
+
+bool QXmppStartTlsPacket::isStartTlsPacket(const QDomElement &element)
+{
+    return element.namespaceURI() == ns_tls && STARTTLS_TYPES.contains(element.tagName());
+}
+
+/// Checks whether the given \p element is a STARTTLS packet according to
+/// <a href="https://xmpp.org/rfcs/rfc6120.html#tls-process-initiate">RFC6120</a>
+/// and has the correct type.
+///
+/// \param element The element that should be checked for being a STARTTLS packet.
+/// \param type The type the element needs to have.
+///
+/// \returns True, if the element is a STARTTLS packet and has the correct type.
+
+bool QXmppStartTlsPacket::isStartTlsPacket(const QDomElement &element, Type type)
+{
+    return element.namespaceURI() == ns_tls && element.tagName() == STARTTLS_TYPES.at(int(type));
+}
---
+++ tests/qxmppstarttlspacket/tst_qxmppstarttlspacket.cpp
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2008-2019 The QXmpp developers
+ *
+ * Author:
+ *  Linus Jahn
+ *
+ * Source:
+ *  https://github.com/qxmpp-project/qxmpp
+ *
+ * This file is a part of QXmpp library.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include <QObject>
+
+#include "QXmppStartTlsPacket.h"
+#include "util.h"
+
+class tst_QXmppStartTlsPacket : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void testBasic_data();
+    void testBasic();
+};
+
+void tst_QXmppStartTlsPacket::testBasic_data()
+{
+    QTest::addColumn<QByteArray>("xml");
+    QTest::addColumn<bool>("valid");
+    QTest::addColumn<QXmppStartTlsPacket::Type>("type");
+
+#define ROW(name, xml, valid, type) \
+    QTest::newRow(name) \
+        << QByteArrayLiteral(xml) \
+        << valid \
+        << type
+
+    ROW("starttls", R"(<starttls xmlns="urn:ietf:params:xml:ns:xmpp-tls"/>)", true, QXmppStartTlsPacket::StartTls);
+    ROW("proceed", R"(<proceed xmlns="urn:ietf:params:xml:ns:xmpp-tls"/>)", true, QXmppStartTlsPacket::Proceed);
+    ROW("failure", R"(<failure xmlns="urn:ietf:params:xml:ns:xmpp-tls"/>)", true, QXmppStartTlsPacket::Failure);
+
+    ROW("invalid-tag", R"(<invalid-tag-name xmlns="urn:ietf:params:xml:ns:xmpp-tls"/>)", false, QXmppStartTlsPacket::StartTls);
+
+#undef ROW
+}
+
+void tst_QXmppStartTlsPacket::testBasic()
+{
+    QFETCH(QByteArray, xml);
+    QFETCH(bool, valid);
+    QFETCH(QXmppStartTlsPacket::Type, type);
+
+    QDomDocument doc;
+    QCOMPARE(doc.setContent(xml, true), true);
+    QCOMPARE(QXmppStartTlsPacket::isStartTlsPacket(doc.documentElement()), valid);
+    QCOMPARE(QXmppStartTlsPacket::isStartTlsPacket(doc.documentElement(), type), valid);
+
+    // test other types return false
+    for (auto testValue : {QXmppStartTlsPacket::StartTls,
+                            QXmppStartTlsPacket::Proceed,
+                            QXmppStartTlsPacket::Failure}) {
+        QCOMPARE(QXmppStartTlsPacket::isStartTlsPacket(doc.documentElement(), testValue), testValue == type && valid);
+    }
+
+    if (valid) {
+        QXmppStartTlsPacket packet;
+        parsePacket(packet, xml);
+        QCOMPARE(packet.type(), type);
+        serializePacket(packet, xml);
+
+        QXmppStartTlsPacket packet2(type);
+        serializePacket(packet2, xml);
+
+        QXmppStartTlsPacket packet3;
+        packet3.setType(type);
+        serializePacket(packet2, xml);
+    }
+}
+
+QTEST_MAIN(tst_QXmppStartTlsPacket)
+#include "tst_qxmppstarttlspacket.moc"
