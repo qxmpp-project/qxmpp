c90ac291d08b8ed748b7dee7033a4d641f677b5b
Linus Jahn <lnj@kaidan.im>
2020-07-02 01:19:58 +0200

    Add QXmppPubSubEvent class

--- src/base/QXmppPubSubEvent.cpp
+++ src/base/QXmppPubSubEvent.cpp
@@ -2,6 +2,7 @@
  * Copyright (C) 2008-2020 The QXmpp developers
  *
  * Author:
+ *  Linus Jahn
  *  Germán Márquez Mejía
  *
  * Source:
@@ -21,125 +22,412 @@
  *
  */
 
-#include <QDomElement>
+#include "QXmppPubSubEvent.h"
 
 #include "QXmppConstants_p.h"
-#include "QXmppPubSubEvent.h"
+#include "QXmppDataForm.h"
+#include "QXmppUtils.h"
+
+#include <QDomElement>
+
+///
+/// \class QXmppPubSubEventBase
+///
+/// The QXmppPubSubEventBase class is an abstract class used for parsing of
+/// generic PubSub event notifications as defined by \xep{0060, Publish-
+/// Subscribe}.
+///
+/// This class cannot be used directly. For a full-featured access to the event
+/// notifications, please use the QXmppPubSubEvent class.
+///
+/// \since QXmpp 1.5
+///
+
+///
+/// \class QXmppPubSubEvent
+///
+/// \brief The QXmppPubSubEvent class represents a PubSub event notification as
+/// defined by \xep{0060, Publish-Subscribe}.
+///
+/// This class has a template parameter that can be used to define the type of
+/// the contained items.
+///
+/// You can use QXmppPubSubEvent::isPubSubItem() to check whether an DOM element
+/// is a &lt;message/&gt; with a PubSub event notification. If you set a special
+/// type as a template parameter, validity of the items will also be checked. To
+/// check for an event notification with items from \xep{0118, User Tune} for
+/// example, you could use the following:
+/// \code
+/// QXmppPubSubEvent<QXmppTuneItem>::isPubSubEvent(element);
+/// \endcode
+///
+/// \ingroup Stanzas
+///
+/// \since QXmpp 1.5
+///
+
+static const QStringList PUBSUB_EVENTS = {
+    QStringLiteral("configuration"),
+    QStringLiteral("delete"),
+    QStringLiteral("items"),
+    QStringLiteral("purge"),
+    QStringLiteral("subscription"),
+};
 
 class QXmppPubSubEventPrivate : public QSharedData
 {
 public:
-    QString nodeName;
-    QList<QXmppPubSubItem> items;
+    QXmppPubSubEventPrivate(QXmppPubSubEventBase::EventType type,
+                            const QString &node);
+
+    QXmppPubSubEventBase::EventType eventType;
+    QString node;
+    QStringList retractIds;
+    QString redirectUri;
+    std::optional<QXmppPubSubSubscription> subscription;
+    std::optional<QXmppDataForm> configurationForm;
 };
+
+QXmppPubSubEventPrivate::QXmppPubSubEventPrivate(QXmppPubSubEventBase::EventType type,
+                                                 const QString &node)
+    : eventType(type), node(node)
+{
+}
 
 ///
 /// Constructs a PubSub event.
 ///
-QXmppPubSubEvent::QXmppPubSubEvent() : d(new QXmppPubSubEventPrivate) {}
-
-///
-/// Constructs a copy of \a other.
-///
-/// \param other
-///
-QXmppPubSubEvent::QXmppPubSubEvent(const QXmppPubSubEvent &other) : d(other.d) {}
-
-QXmppPubSubEvent::~QXmppPubSubEvent() {}
-
-///
-/// Assigns \a other to this event.
-///
-/// \param other
-///
-QXmppPubSubEvent &QXmppPubSubEvent::operator=(const QXmppPubSubEvent &other)
+QXmppPubSubEventBase::QXmppPubSubEventBase(EventType type, const QString &node)
+    : d(new QXmppPubSubEventPrivate(type, node))
 {
-    d = other.d;
-    return *this;
+    setType(QXmppMessage::Normal);
+}
+
+/// Default copy-constructor.
+QXmppPubSubEventBase::QXmppPubSubEventBase(const QXmppPubSubEventBase &other) = default;
+
+QXmppPubSubEventBase::~QXmppPubSubEventBase() = default;
+
+/// Default assignment operator.
+QXmppPubSubEventBase &QXmppPubSubEventBase::operator=(const QXmppPubSubEventBase &other) = default;
+
+///
+/// Returns the event type of the PubSub event.
+///
+QXmppPubSubEventBase::EventType QXmppPubSubEventBase::eventType() const
+{
+    return d->eventType;
+}
+
+///
+/// Sets the event type of the PubSub event.
+///
+void QXmppPubSubEventBase::setEventType(EventType type)
+{
+    d->eventType = type;
 }
 
 ///
 /// Returns the name of the event's node.
 ///
-/// \return the node name
+/// This does not work with Subscription events. In those cases you need to get
+/// the node of the subscription.
 ///
-QString QXmppPubSubEvent::nodeName() const
+/// \sa subscription()
+/// \sa QXmppPubSubSubscription::node()
+///
+QString QXmppPubSubEventBase::node() const
 {
-    return d->nodeName;
+    return d->node;
 }
 
 ///
 /// Sets the name of the event's node.
 ///
-/// \param nodeName
+/// This does not work with Subscription events. In those cases you need to set
+/// the node of the subscription.
 ///
-void QXmppPubSubEvent::setNodeName(const QString &nodeName)
+/// \sa subscription()
+/// \sa QXmppPubSubSubscription::setNode()
+///
+void QXmppPubSubEventBase::setNode(const QString &node)
 {
-    d->nodeName = nodeName;
+
+    d->node = node;
 }
 
 ///
-/// Returns the PubSub items of the event.
+/// Returns the item IDs that have been retracted.
 ///
-/// \return a list of the PubSub items
+/// This is only used for the Items event type.
 ///
-QList<QXmppPubSubItem> QXmppPubSubEvent::items() const
+QStringList QXmppPubSubEventBase::retractIds() const
 {
-    return d->items;
+    return d->retractIds;
 }
 
 ///
-/// Sets the PubSub items of the event.
+/// Sets the item IDs that have been retracted.
 ///
-/// \param items
+/// This is only used for the Items event type.
 ///
-void QXmppPubSubEvent::setItems(const QList<QXmppPubSubItem> &items)
+void QXmppPubSubEventBase::setRetractIds(const QStringList &retractIds)
 {
-    d->items = items;
+    d->retractIds = retractIds;
 }
 
 ///
-/// Determines whether the event object is initialized.
+/// Returns the redirect URI to the new node.
 ///
-/// \return true if the node name is empty and the event contains no items
+/// This can be set for delete notifications to inform subscribers of the new
+/// node. Inclusion of this is of course optional.
 ///
-bool QXmppPubSubEvent::isNull() const
+QString QXmppPubSubEventBase::redirectUri() const
 {
-    return d->nodeName.isEmpty() && d->items.isEmpty();
+    return d->redirectUri;
+}
+
+///
+/// Sets the redirect URI to the new node.
+///
+/// This can be set for delete notifications to inform subscribers of the new
+/// node. Inclusion of this is of course optional.
+///
+void QXmppPubSubEventBase::setRedirectUri(const QString &redirectUri)
+{
+    d->redirectUri = redirectUri;
+}
+
+///
+/// Returns the subscription in case of a Subscription event.
+///
+std::optional<QXmppPubSubSubscription> QXmppPubSubEventBase::subscription() const
+{
+    return d->subscription;
+}
+
+///
+/// Sets the subscription in case of a Subscription event.
+///
+void QXmppPubSubEventBase::setSubscription(const std::optional<QXmppPubSubSubscription> &subscription)
+{
+    d->subscription = subscription;
+}
+
+///
+/// Returns a configuration data form if the event contains one.
+///
+std::optional<QXmppDataForm> QXmppPubSubEventBase::configurationForm() const
+{
+    return d->configurationForm;
+}
+
+///
+/// Sets a configuration data form (or clears it with std::nullopt).
+///
+void QXmppPubSubEventBase::setConfigurationForm(const std::optional<QXmppDataForm> &configurationForm)
+{
+    d->configurationForm = configurationForm;
 }
 
 /// \cond
-void QXmppPubSubEvent::parse(const QDomElement &element)
+bool QXmppPubSubEventBase::isPubSubEvent(const QDomElement &stanza, std::function<bool(const QDomElement &)> isItemValid)
 {
-    // parse node name
-    QDomElement itemsElement = element.firstChildElement("items");
-    d->nodeName = itemsElement.attribute("node");
+    if (stanza.tagName() != QStringLiteral("message"))
+        return false;
 
-    // parse items
-    QDomElement childElement = itemsElement.firstChildElement("item");
-    while (!childElement.isNull())
-    {
-        QXmppPubSubItem item;
-        item.parse(childElement);
-        d->items << item;
-        childElement = childElement.nextSiblingElement("item");
+    // find correct "event" element
+    auto event = stanza.firstChildElement(QStringLiteral("event"));
+    while (true) {
+        if (event.isNull())
+            return false;
+        if (event.namespaceURI() == ns_pubsub_event)
+            break;
+
+        event = event.nextSiblingElement(QStringLiteral("event"));
     }
+
+    auto eventTypeElement = event.firstChildElement();
+
+    // check for validity of the event type
+    int eventType = PUBSUB_EVENTS.indexOf(eventTypeElement.tagName());
+    if (eventType == -1)
+        return false;
+
+    // check for "node" attribute when required
+    switch (EventType(eventType)) {
+    case Delete:
+    case Items:
+    case Purge:
+        if (!eventTypeElement.hasAttribute(QStringLiteral("node")))
+            return false;
+        break;
+    case Configuration:
+    case Subscription:
+        break;
+    }
+
+    // check individual content
+    switch (EventType(eventType)) {
+    case Delete: {
+        auto redirect = eventTypeElement.firstChildElement(QStringLiteral("redirect"));
+        if (!redirect.isNull() && !redirect.hasAttribute(QStringLiteral("uri")))
+            return false;
+        break;
+    }
+    case Items: {
+        // check validity of the items using isItemValid()
+        for (auto itemElement = eventTypeElement.firstChildElement(QStringLiteral("item"));
+             !itemElement.isNull();
+             itemElement = itemElement.nextSiblingElement(QStringLiteral("item"))) {
+            if (!isItemValid(itemElement))
+                return false;
+        }
+        break;
+    }
+    case Subscription: {
+        if (!QXmppPubSubSubscription::isSubscription(eventTypeElement))
+            return false;
+    }
+    case Configuration:
+    case Purge:
+        break;
+    }
+
+    return true;
 }
 
-void QXmppPubSubEvent::toXml(QXmlStreamWriter *writer) const
+bool QXmppPubSubEventBase::parseExtension(const QDomElement &eventElement)
 {
-    writer->writeStartElement("event");
-    writer->writeAttribute("xmlns", ns_pubsub_event);
+    if (eventElement.tagName() == QStringLiteral("event") && eventElement.namespaceURI() == ns_pubsub_event) {
+        // check that the query type is valid
+        const QDomElement eventTypeElement = eventElement.firstChildElement();
+        int eventType = PUBSUB_EVENTS.indexOf(eventTypeElement.tagName());
+        if (eventType == -1)
+            return false;
+        d->eventType = EventType(eventType);
 
-    // write node name
-    writer->writeStartElement("items");
-    writer->writeAttribute("node", d->nodeName);
+        // parse "node" attribute
+        switch (EventType(eventType)) {
+        case Configuration:
+        case Delete:
+        case Items:
+        case Purge:
+            d->node = eventTypeElement.attribute(QStringLiteral("node"));
+            break;
+        case Subscription:
+            break;
+        }
 
-    // write items
-    for (const QXmppPubSubItem &item : d->items)
-        item.toXml(writer);
+        // check the items using isItemValid()
+        switch (d->eventType) {
+        case Delete: {
+            auto redirect = eventTypeElement.firstChildElement(QStringLiteral("redirect"));
+            if (!redirect.isNull()) {
+                d->redirectUri = redirect.attribute(QStringLiteral("uri"));
+            }
+            break;
+        }
+        case Items: {
+            // parse items
+            parseItems(eventTypeElement);
 
-    writer->writeEndElement();
-    writer->writeEndElement();
+            // parse retract ids
+            auto retract = eventTypeElement.firstChildElement(QStringLiteral("retract"));
+            while (!retract.isNull()) {
+                d->retractIds << retract.attribute(QStringLiteral("id"));
+
+                retract = retract.nextSiblingElement(QStringLiteral("retract"));
+            }
+
+            break;
+        }
+        case Subscription: {
+            QXmppPubSubSubscription subscription;
+            subscription.parse(eventTypeElement);
+            d->subscription = subscription;
+            break;
+        }
+        case Configuration: {
+            auto forms = eventTypeElement.elementsByTagNameNS(ns_data, QStringLiteral("x"));
+            if (!forms.isEmpty()) {
+                QXmppDataForm form;
+                form.parse(forms.at(0).toElement());
+                d->configurationForm = form;
+            }
+            break;
+        }
+        case Purge:
+            break;
+        }
+    } else {
+        // handles QXmppMessage default extensions
+        return QXmppMessage::parseExtension(eventElement);
+    }
+
+    return true;
+}
+
+void QXmppPubSubEventBase::serializeExtensions(QXmlStreamWriter *writer) const
+{
+    QXmppMessage::serializeExtensions(writer);
+
+    writer->writeStartElement(QStringLiteral("event"));
+    writer->writeDefaultNamespace(ns_pubsub_event);
+
+    if (d->eventType == Subscription && d->subscription) {
+        d->subscription->toXml(writer);
+    } else {
+        writer->writeStartElement(PUBSUB_EVENTS.at(int(d->eventType)));
+
+        // write node attribute
+        switch (d->eventType) {
+        case Delete:
+        case Items:
+        case Purge:
+            // node attribute is required
+            writer->writeAttribute(QStringLiteral("node"), d->node);
+            break;
+        case Configuration:
+            // node attribute is optional
+            helperToXmlAddAttribute(writer, QStringLiteral("node"), d->node);
+            break;
+        case Subscription:
+            break;
+        }
+
+        switch (d->eventType) {
+        case Configuration:
+            if (d->configurationForm) {
+                d->configurationForm->toXml(writer);
+            }
+            break;
+        case Delete:
+            if (!d->redirectUri.isEmpty()) {
+                writer->writeStartElement(QStringLiteral("redirect"));
+                writer->writeAttribute(QStringLiteral("uri"), d->redirectUri);
+                writer->writeEndElement();
+            }
+        case Items:
+            // serialize items
+            serializeItems(writer);
+
+            // serialize retract ids
+            for (const auto &id : qAsConst(d->retractIds)) {
+                writer->writeStartElement(QStringLiteral("retract"));
+                writer->writeAttribute(QStringLiteral("id"), id);
+                writer->writeEndElement();
+            }
+
+            break;
+        case Purge:
+        case Subscription:
+            break;
+        }
+
+        writer->writeEndElement();  // close event's type element
+    }
+
+    writer->writeEndElement();  // </event>
 }
 /// \endcond
--- src/base/QXmppPubSubEvent.h
+++ src/base/QXmppPubSubEvent.h
@@ -2,6 +2,7 @@
  * Copyright (C) 2008-2020 The QXmpp developers
  *
  * Author:
+ *  Linus Jahn
  *  Germán Márquez Mejía
  *
  * Source:
@@ -24,41 +25,143 @@
 #ifndef QXMPPPUBSUBEVENT_H
 #define QXMPPPUBSUBEVENT_H
 
+#include "QXmppMessage.h"
+#include "QXmppPubSubSubscription.h"
+
+#include <functional>
+
+#include <QDomElement>
 #include <QSharedData>
 
-#include "QXmppElement.h"
-#include "QXmppPubSubItem.h"
-
+class QXmppDataForm;
 class QXmppPubSubEventPrivate;
+class QXmppPubSubItem;
 
-///
-/// \brief The QXmppPubSubEvent class represents a publish-subscribe event
-/// as defined by XEP-0060: Publish-Subscribe.
-///
-class QXMPP_EXPORT QXmppPubSubEvent
+class QXMPP_EXPORT QXmppPubSubEventBase : public QXmppMessage
 {
 public:
-    QXmppPubSubEvent();
-    QXmppPubSubEvent(const QXmppPubSubEvent &other);
-    ~QXmppPubSubEvent();
+    ///
+    /// Enumeration of different event types
+    ///
+    enum EventType {
+        Configuration,
+        Delete,
+        Items,
+        Purge,
+        Subscription,
+    };
 
-    QXmppPubSubEvent& operator=(const QXmppPubSubEvent &other);
+    QXmppPubSubEventBase(EventType = Items, const QString &node = {});
+    QXmppPubSubEventBase(const QXmppPubSubEventBase &other);
+    virtual ~QXmppPubSubEventBase();
 
-    QString nodeName() const;
-    void setNodeName(const QString &nodeName);
+    QXmppPubSubEventBase &operator=(const QXmppPubSubEventBase &other);
 
-    QList<QXmppPubSubItem> items() const;
-    void setItems(const QList<QXmppPubSubItem> &items);
+    EventType eventType() const;
+    void setEventType(EventType);
 
-    bool isNull() const;
+    QString node() const;
+    void setNode(const QString &node);
 
+    QStringList retractIds() const;
+    void setRetractIds(const QStringList &);
+
+    QString redirectUri() const;
+    void setRedirectUri(const QString &);
+
+    std::optional<QXmppPubSubSubscription> subscription() const;
+    void setSubscription(const std::optional<QXmppPubSubSubscription> &subscription);
+
+    std::optional<QXmppDataForm> configurationForm() const;
+    void setConfigurationForm(const std::optional<QXmppDataForm> &configurationForm);
+
+protected:
     /// \cond
-    void parse(const QDomElement &element);
-    void toXml(QXmlStreamWriter *writer) const;
+    static bool isPubSubEvent(const QDomElement &element, std::function<bool(const QDomElement &)> isItemValid);
+
+    bool parseExtension(const QDomElement &element) override;
+    void serializeExtensions(QXmlStreamWriter *writer) const override;
+
+    virtual void parseItems(const QDomElement &) = 0;
+    virtual void serializeItems(QXmlStreamWriter *writer) const = 0;
     /// \endcond
 
 private:
     QSharedDataPointer<QXmppPubSubEventPrivate> d;
 };
+
+template<class T = QXmppPubSubItem>
+class QXmppPubSubEvent : public QXmppPubSubEventBase
+{
+public:
+    QList<T> items() const;
+    void setItems(const QList<T> &items);
+
+    static bool isPubSubEvent(const QDomElement &element);
+
+protected:
+    /// \cond
+    void parseItems(const QDomElement &) override;
+    void serializeItems(QXmlStreamWriter *writer) const override;
+    /// \endcond
+
+private:
+    QList<T> m_items;
+};
+
+///
+/// Returns the PubSub items of the event.
+///
+template<class T>
+QList<T> QXmppPubSubEvent<T>::items() const
+{
+    return m_items;
+}
+
+///
+/// Sets the PubSub items of the event.
+///
+template<class T>
+void QXmppPubSubEvent<T>::setItems(const QList<T> &items)
+{
+    m_items = items;
+}
+
+///
+/// Returns whether the element is a valid QXmppPubSubEvent and contains only
+/// valid items of type T.
+///
+template<class T>
+bool QXmppPubSubEvent<T>::isPubSubEvent(const QDomElement &element)
+{
+    return QXmppPubSubEventBase::isPubSubEvent(element, [](const QDomElement &element) {
+        return T::isItem(element);
+    });
+}
+
+/// \cond
+template<class T>
+void QXmppPubSubEvent<T>::parseItems(const QDomElement &parent)
+{
+    QDomElement child = parent.firstChildElement(QStringLiteral("item"));
+    while (!child.isNull()) {
+        T item;
+        item.parse(child);
+        m_items << item;
+
+        child = child.nextSiblingElement(QStringLiteral("item"));
+    }
+}
+
+template<class T>
+void QXmppPubSubEvent<T>::serializeItems(QXmlStreamWriter *writer) const
+{
+    for (const auto &item : qAsConst(m_items)) {
+        item.toXml(writer);
+    }
+}
+/// \endcond
+
+Q_DECLARE_METATYPE(QXmppPubSubEventBase::EventType)
 
 #endif // QXMPPPUBSUBEVENT_H
