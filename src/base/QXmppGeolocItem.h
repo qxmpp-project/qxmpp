8a4a9bbb7147c0c93f02e797d22a41070992fa07
Cochise César <cochisecesar@zoho.com>
2022-01-10 01:27:06 -0300

    Implement XEP-0080: User Location

---
+++ src/base/QXmppGeolocItem.h
@@ -0,0 +1,54 @@
+// SPDX-FileCopyrightText: 2022 Cochise César <cochisecesar@zoho.com>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#ifndef QXMPPGEOLOCITEM_H
+#define QXMPPGEOLOCITEM_H
+
+#include "QXmppPubSubItem.h"
+
+#include <optional>
+
+#include <QSharedDataPointer>
+
+class QXmppGeolocItemPrivate;
+
+class QXMPP_EXPORT QXmppGeolocItem : public QXmppPubSubItem
+{
+public:
+    QXmppGeolocItem();
+    QXmppGeolocItem(const QXmppGeolocItem &other);
+    ~QXmppGeolocItem();
+
+    QXmppGeolocItem &operator=(const QXmppGeolocItem &other);
+
+    std::optional<double> accuracy() const;
+    void setAccuracy(std::optional<double> accuracy);
+
+    QString country() const;
+    void setCountry(QString country);
+
+    std::optional<double> latitude() const;
+    void setLatitude(std::optional<double> lat);
+
+    QString locality() const;
+    void setLocality(QString locality);
+
+    std::optional<double> longitude() const;
+    void setLongitude(std::optional<double> lon);
+
+    static bool isItem(const QDomElement &itemElement);
+
+protected:
+    /// \cond
+    void parsePayload(const QDomElement &payloadElement) override;
+    void serializePayload(QXmlStreamWriter *writer) const override;
+    /// \endcond
+
+private:
+    QSharedDataPointer<QXmppGeolocItemPrivate> d;
+};
+
+Q_DECLARE_METATYPE(QXmppGeolocItem)
+
+#endif  // QXMPPGEOLOCITEM_H
