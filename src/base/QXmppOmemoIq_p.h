78b7bff9185f53fb3acd61ed41af964aeae1e0d5
Melvin Keskin <melvo@olomono.de>
2022-01-27 17:06:12 +0100

    Add QXmppOmemoIq
    The IQ is only used internally and is private.

---
+++ src/base/QXmppOmemoIq_p.h
@@ -0,0 +1,30 @@
+// SPDX-FileCopyrightText: 2022 Melvin Keskin <melvo@olomono.de>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#ifndef QXMPPOMEMOIQ_H
+#define QXMPPOMEMOIQ_H
+
+#include "QXmppIq.h"
+#include "QXmppOmemoElement.h"
+
+class QXmppOmemoIqPrivate;
+
+class QXmppOmemoIq : public QXmppIq
+{
+public:
+    QXmppOmemoElement omemoElement();
+    void setOmemoElement(const QXmppOmemoElement &omemoElement);
+
+    /// \cond
+    void parseElementFromChild(const QDomElement &element) override;
+    void toXmlElementFromChild(QXmlStreamWriter *writer) const override;
+    /// \endcond
+
+    static bool isOmemoIq(const QDomElement &element);
+
+private:
+    QXmppOmemoElement m_omemoElement;
+};
+
+#endif  // QXMPPOMEMOIQ_H
