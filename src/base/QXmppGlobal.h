66b718973a9d32cecdabf1c54799e605fca00fbb
Melvin Keskin <melvo@olomono.de>
2022-03-26 12:00:17 +0100

    E2eeMetadata: Add enum and attribute for the used encryption

--- src/base/QXmppGlobal.h
+++ src/base/QXmppGlobal.h
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2010 Manjeet Dahiya <manjeetdahiya@gmail.com>
 // SPDX-FileCopyrightText: 2017 Niels Ole Salscheider <niels_ole@salscheider-online.de>
 // SPDX-FileCopyrightText: 2019 Linus Jahn <lnj@kaidan.im>
+// SPDX-FileCopyrightText: 2022 Melvin Keskin <melvo@olomono.de>
 //
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
@@ -47,6 +48,50 @@
 /// \since QXmpp 1.5
 ///
 namespace QXmpp {
+
+///
+/// This enum describes different end-to-end encryption methods. These can
+/// be used to mark a stanza as encrypted or decrypted with a specific algorithm
+/// (e.g., for \xep{0380, Explicit Message Encryption}).
+///
+/// \since QXmpp 1.5
+///
+enum Encryption {
+    /// No encryption
+    NoEncryption,
+    /// Unknown encryption
+    UnknownEncryption,
+    /// \xep{0364, Current Off-the-Record Messaging Usage}
+    Otr,
+    /// \xep{0027, Current Jabber OpenPGP Usage}
+    LegacyOpenPgp,
+    /// \xep{0373, OpenPGP for XMPP}
+    Ox,
+    /// \xep{0384, OMEMO Encryption}
+    Omemo,
+    /// \xep{0384, OMEMO Encryption} since version 0.4
+    Omemo1,
+    /// \xep{0384, OMEMO Encryption} since version 0.8
+    Omemo2,
+#if QXMPP_DEPRECATED_SINCE(1, 5)
+    /// \xep{0364, Current Off-the-Record Messaging Usage}
+    /// \deprecated This enum is deprecated since QXmpp 1.5. Use
+    /// \c QXmpp::Otr instead.
+    OTR = Otr,
+    /// \xep{0027, Current Jabber OpenPGP Usage}
+    /// \deprecated This enum is deprecated since QXmpp 1.5. Use
+    /// \c QXmpp::LegacyOpenPgp instead.
+    LegacyOpenPGP = LegacyOpenPgp,
+    /// \xep{0373, OpenPGP for XMPP}
+    /// \deprecated This enum is deprecated since QXmpp 1.5. Use
+    /// \c QXmpp::Ox instead.
+    OX = Ox,
+    /// \xep{0384, OMEMO Encryption}
+    /// \deprecated This enum is deprecated since QXmpp 1.5. Use
+    /// \c QXmpp::Omemo instead.
+    OMEMO = Omemo,
+#endif
+};
 
 ///
 /// Parsing/serialization mode when using Stanza Content Encryption.
