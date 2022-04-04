a060a70cf6a815b18dc42ed76a3a577cc3ec14e2
Linus Jahn <lnj@kaidan.im>
2022-04-04 12:00:16 +0200

    Rename QXmppGlobal to QXmppBuildConstants
    Plan is to split up Global into build constants and other globals. This
    avoids hassle when editing QXmppGlobal. With IDEs it can easily happen
    that you edit the wrong file from the build dir instead the `.h.in`.
    
    Co-authored-by: Melvin Keskin <melvo@olomono.de>

---
+++ src/base/QXmppGlobal.h
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2010 Manjeet Dahiya <manjeetdahiya@gmail.com>
+// SPDX-FileCopyrightText: 2017 Niels Ole Salscheider <niels_ole@salscheider-online.de>
+// SPDX-FileCopyrightText: 2019 Linus Jahn <lnj@kaidan.im>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#ifndef QXMPPGLOBAL_H
+#define QXMPPGLOBAL_H
+
+#include "QXmppBuildConstants.h"
+
+#endif  // QXMPPGLOBAL_H
