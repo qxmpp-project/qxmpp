2ffad26f7eae87d94f90b814f2e49c002f558032
Linus Jahn <lnj@kaidan.im>
2022-04-06 19:00:22 +0200

    Add generic QXmppExtension

---
+++ src/base/QXmppExtension.h
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2022 Linus Jahn <lnj@kaidan.im>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#ifndef QXMPPEXTENSION_H
+#define QXMPPEXTENSION_H
+
+#include "QXmppGlobal.h"
+
+class QXMPP_EXPORT QXmppExtension
+{
+};
+
+#endif  // QXMPPEXTENSION_H
