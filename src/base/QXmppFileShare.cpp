a9997f165304007e8224a3560b2eb337782bcfb7
Linus Jahn <lnj@kaidan.im>
2022-09-05 19:20:39 +0200

    Implement XEP-0447: Stateless file sharing: File sharing element
    Implements parsing for the file sharing element from XEP-0447:
    Stateless files sharing version 0.2.
    
    https://xmpp.org/extensions/xep-0447.html

---
+++ src/base/QXmppFileShare.cpp
@@ -0,0 +1,147 @@
+// SPDX-FileCopyrightText: 2022 Linus Jahn <lnj@kaidan.im>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#include "QXmppFileShare.h"
+
+#include <optional>
+#include <QDomElement>
+#include <QXmlStreamWriter>
+#include "QXmppConstants_p.h"
+
+using Disposition = QXmppFileShare::Disposition;
+
+static std::optional<Disposition> dispositionFromString(const QString &str)
+{
+    if (str == "inline") {
+        return Disposition::Inline;
+    }
+    if (str == "attachment") {
+        return Disposition::Attachment;
+    }
+    return {};
+}
+
+static QString dispositionToString(Disposition value)
+{
+    switch (value) {
+    case Disposition::Inline:
+        return QStringLiteral("inline");
+    case Disposition::Attachment:
+        return QStringLiteral("attachment");
+    }
+    Q_UNREACHABLE();
+}
+
+/// \cond
+class QXmppFileSharePrivate : public QSharedData
+{
+public:
+    QXmppFileMetadata metadata;
+    QVector<QUrl> httpSources;
+    QXmppFileShare::Disposition disposition = Disposition::Inline;
+};
+/// \endcond
+
+///
+/// \class QXmppFileSharePrivate
+///
+/// File sharing element from \xep{0447, Stateless file sharing}. Contains
+/// metadata and source URLs.
+///
+/// \note jinglepub references are currently missing
+///
+/// \since QXmpp 1.5
+///
+
+/// Default constructor
+QXmppFileShare::QXmppFileShare()
+    : d(new QXmppFileSharePrivate)
+{
+}
+
+QXmppFileShare::QXmppFileShare(const QXmppFileShare &) = default;
+QXmppFileShare::QXmppFileShare(QXmppFileShare &&) noexcept = default;
+QXmppFileShare::~QXmppFileShare() = default;
+QXmppFileShare &QXmppFileShare::operator=(const QXmppFileShare &) = default;
+QXmppFileShare &QXmppFileShare::operator=(QXmppFileShare &&) noexcept = default;
+
+/// Returns the disposition setting for this file.
+QXmppFileShare::Disposition QXmppFileShare::disposition() const
+{
+    return d->disposition;
+}
+
+/// Sets the disposition setting for this file.
+void QXmppFileShare::setDisposition(Disposition disp)
+{
+    d->disposition = disp;
+}
+
+const QXmppFileMetadata &QXmppFileShare::metadata() const
+{
+    return d->metadata;
+}
+
+void QXmppFileShare::setMetadata(const QXmppFileMetadata &metadata)
+{
+    d->metadata = metadata;
+}
+
+const QVector<QUrl> &QXmppFileShare::httpSources() const
+{
+    return d->httpSources;
+}
+
+void QXmppFileShare::setHttpSources(const QVector<QUrl> &newHttpSources)
+{
+    d->httpSources = newHttpSources;
+}
+
+/// \cond
+bool QXmppFileShare::parse(const QDomElement &el)
+{
+    if (el.tagName() == "file-sharing" && el.namespaceURI() == ns_sfs) {
+        // disposition
+        d->disposition = dispositionFromString(el.attribute("disposition"))
+                .value_or(Disposition::Inline);
+
+        // file metadata
+        auto fileEl = el.firstChildElement("file");
+        d->metadata = QXmppFileMetadata();
+        if (!d->metadata.parse(fileEl)) {
+            return false;
+        }
+
+        // sources:
+        // expect that there's only one sources element with the correct namespace
+        auto sources = el.firstChildElement("sources");
+        for (auto urlEl = sources.firstChildElement("url-data");
+             !urlEl.isNull();
+             urlEl = urlEl.nextSiblingElement("url-data")) {
+            if (urlEl.namespaceURI() == "http://jabber.org/protocol/url-data") {
+                d->httpSources.append(QUrl(urlEl.attribute("target")));
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+void QXmppFileShare::toXml(QXmlStreamWriter *writer) const
+{
+    writer->writeStartElement("file-sharing");
+    writer->writeDefaultNamespace(ns_sfs);
+    writer->writeAttribute("disposition", dispositionToString(d->disposition));
+    d->metadata.toXml(writer);
+    writer->writeStartElement("sources");
+    for (const auto &source : d->httpSources) {
+        writer->writeStartElement("url-data");
+        writer->writeDefaultNamespace(ns_url_data);
+        writer->writeAttribute("target", source.toString());
+        writer->writeEndElement();
+    }
+    writer->writeEndElement();
+    writer->writeEndElement();
+}
+/// \endcond
---
+++ src/base/QXmppFileShare.h
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: 2022 Linus Jahn <lnj@kaidan.im>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#ifndef QXMPPFILESHARE_H
+#define QXMPPFILESHARE_H
+
+#include <QUrl>
+#include <QSharedDataPointer>
+
+class QDomElement;
+class QXmlStreamWriter;
+class QXmppFileSharePrivate;
+class QXmppFileMetadata;
+
+class QXmppFileShare
+{
+public:
+    enum Disposition {
+        Inline,
+        Attachment,
+    };
+
+    QXmppFileShare();
+    QXmppFileShare(const QXmppFileShare &);
+    QXmppFileShare(QXmppFileShare &&) noexcept;
+    ~QXmppFileShare();
+
+    QXmppFileShare &operator=(const QXmppFileShare &);
+    QXmppFileShare &operator=(QXmppFileShare &&) noexcept;
+
+    Disposition disposition() const;
+    void setDisposition(Disposition);
+
+    const QXmppFileMetadata &metadata() const;
+    void setMetadata(const QXmppFileMetadata &);
+
+    const QVector<QUrl> &httpSources() const;
+    void setHttpSources(const QVector<QUrl> &newHttpSources);
+
+    /// \cond
+    bool parse(const QDomElement &el);
+    void toXml(QXmlStreamWriter *writer) const;
+    /// \endcond
+    
+private:
+    QSharedDataPointer<QXmppFileSharePrivate> d;
+};
+
+#endif // QXMPPFILESHARE_H
