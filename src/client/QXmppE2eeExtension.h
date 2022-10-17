3a8c3b4cdb3868b7b26e40a09d2986f8109a3b96
Linus Jahn <lnj@kaidan.im>
2022-10-17 16:14:35 +0200

    E2eeExtension: Add isEncrypted() and decryptMessage() functions

--- src/client/QXmppE2eeExtension.h
+++ src/client/QXmppE2eeExtension.h
@@ -5,6 +5,7 @@
 #ifndef QXMPPE2EEEXTENSION_H
 #define QXMPPE2EEEXTENSION_H
 
+#include "QXmppError.h"
 #include "QXmppExtension.h"
 #include "QXmppSendResult.h"
 #include "QXmppSendStanzaParams.h"
@@ -25,13 +26,16 @@ public:
     };
 
     using MessageEncryptResult = std::variant<QByteArray, QXmpp::SendError>;
+    using MessageDecryptResult = std::variant<QXmppMessage, NotEncrypted, QXmppError>;
     using IqEncryptResult = std::variant<QByteArray, QXmpp::SendError>;
     using IqDecryptResult = std::variant<QDomElement, NotEncrypted, QXmpp::SendError>;
 
     virtual QFuture<MessageEncryptResult> encryptMessage(QXmppMessage &&, const std::optional<QXmppSendStanzaParams> &) = 0;
-
+    virtual QFuture<MessageDecryptResult> decryptMessage(QXmppMessage &&) = 0;
     virtual QFuture<IqEncryptResult> encryptIq(QXmppIq &&, const std::optional<QXmppSendStanzaParams> &) = 0;
     virtual QFuture<IqDecryptResult> decryptIq(const QDomElement &) = 0;
+    virtual bool isEncrypted(const QDomElement &) = 0;
+    virtual bool isEncrypted(const QXmppMessage &) = 0;
 };
 
 #endif  // QXMPPE2EEEXTENSION_H
