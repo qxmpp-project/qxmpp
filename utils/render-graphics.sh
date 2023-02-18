92dcb72c917a946755cbdf59c91dffc2a554e9e8
Melvin Keskin <melvo@olomono.de>
2023-02-18 17:50:28 +0100

    Add rendering script for logo with margins e.g. for the chat avatar
    Variants of the logo inlcuding specific margins can be generated.
    That is needed if the logo is used without the possibility to dynamically
    adjust its margins.

---
+++ utils/render-graphics.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2023 Melvin Keskin <melvo@olomono.de>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+QXMPP_SOURCES=$(dirname "$(readlink -f "${0}")")/..
+
+echo "*****************************************"
+echo "Rendering graphics"
+echo "*****************************************"
+
+render_support_chat_avatar() {
+    render_svg_with_margin $QXMPP_SOURCES/logo.svg $QXMPP_SOURCES/support-chat-avatar.png 300 300 18
+}
+
+# $1 - $4: see render_svg()
+# $5: margin
+render_svg_with_margin() {
+    output_directory=$(dirname $2)
+    mkdir -p $output_directory
+    tmp_file=$output_directory/rendered_tmp.svg
+    inkscape -o $tmp_file --export-margin=$5 $1
+    render_svg $tmp_file $2 $3 $4
+    rm $tmp_file
+}
+
+# $1: input file
+# $2: output file
+# $3: width
+# $4: height
+render_svg() {
+    inkscape -o $2 -w $3 -h $4 $1 >/dev/null
+    optipng -quiet -o7 $2 >/dev/null
+    advpng -z4 $2 >/dev/null
+    echo "Created "$2
+}
+
+render_support_chat_avatar
