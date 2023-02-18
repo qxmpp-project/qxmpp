2631115e763b0972acf849ee76503a3ea88b9b7f
Linus Jahn <lnj@kaidan.im>
2023-02-18 14:06:38 +0100

    FileSharingManager: Format code in a clang-format compatible way

--- src/client/QXmppFileSharingManager.cpp
+++ src/client/QXmppFileSharingManager.cpp
@@ -36,15 +36,11 @@ using MetadataGeneratorResult = QXmppFileSharingManager::MetadataGeneratorResult
 // The manager generates a hash with each hash algorithm
 static std::vector<HashAlgorithm> hashAlgorithms()
 {
-    return
-    {
-        HashAlgorithm::Sha256,
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-            HashAlgorithm::Blake2b_256,
+    return { HashAlgorithm::Sha256, HashAlgorithm::Blake2b_256 };
 #else
-            HashAlgorithm::Sha3_256,
+    return { HashAlgorithm::Sha256, HashAlgorithm::Sha3_256 };
 #endif
-    };
 }
 
 template<typename T, typename Converter>
