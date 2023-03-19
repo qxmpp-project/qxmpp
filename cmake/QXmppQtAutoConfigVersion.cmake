7d1e74b5147f411d30f2d76420ba62737ec93ab9
Linus Jahn <lnj@kaidan.im>
2023-03-19 01:48:29 +0100

    Support installing QXmpp with Qt 5 and Qt 6 in parallel
    Closes #540.

---
+++ cmake/QXmppQtAutoConfigVersion.cmake
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023 Linus Jahn <lnj@kaidan.im>
+#
+# SPDX-License-Identifier: CC0-1.0
+
+set(PACKAGE_VERSION "")
+# Allow all requested versions here, the actual check is going to be done in QXmppQt5/6.
+set(PACKAGE_VERSION_COMPATIBLE TRUE)
+
