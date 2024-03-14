3ea6b1e4ebdf5e44a3c0664ef716eb610ddc748f
Linus Jahn <lnj@kaidan.im>
2024-03-14 20:35:44 +0100

    Introduce AuthenticationError struct

---
+++ src/client/QXmppAuthenticationError.h
@@ -0,0 +1,46 @@
+// SPDX-FileCopyrightText: 2024 Linus Jahn <lnj@kaidan.im>
+//
+// SPDX-License-Identifier: LGPL-2.1-or-later
+
+#ifndef QXMPPAUTHENTICATIONERROR_H
+#define QXMPPAUTHENTICATIONERROR_H
+
+#include "QXmppGlobal.h"
+
+#include <any>
+
+namespace QXmpp {
+
+///
+/// Indicates an authentication error
+///
+/// \since QXmpp 1.7
+///
+struct AuthenticationError {
+    /// Describes the type of the authentication error.
+    enum Type {
+        /// The provided credentials have been rejected by the server.
+        NotAuthorized,
+        /// The server did not allow authentication because the account is currently disabled.
+        AccountDisabled,
+        /// Used credentials are not valid anymore.
+        CredentialsExpired,
+        /// Authentication is only allowed with an encrypted connection.
+        EncryptionRequired,
+        /// Authenticated could not be completed because the server did not offer a compatible
+        /// authentication mechanism.
+        MechanismMismatch,
+        /// Local error while processing authentication challenges
+        ProcessingError,
+    };
+    /// Type of the authentication error
+    Type type;
+    /// Error message from the server
+    QString text;
+    /// Protocol-specific details provided to the error
+    std::any details;
+};
+
+}  // namespace QXmpp
+
+#endif  // QXMPPAUTHENTICATIONERROR_H
