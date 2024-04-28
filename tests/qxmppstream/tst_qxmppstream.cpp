cc350a507a8940a2e2911ca05fa7e52eb7277d3b
Linus Jahn <lnj@kaidan.im>
2024-04-28 14:33:27 +0200

    tests: Move starttlspacket test into stream test for compilation speed

--- tests/qxmppstream/tst_qxmppstream.cpp
+++ tests/qxmppstream/tst_qxmppstream.cpp
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 #include "QXmppConstants_p.h"
+#include "QXmppStartTlsPacket.h"
 #include "QXmppStream.h"
 #include "QXmppStreamError_p.h"
 
@@ -62,6 +63,10 @@ private:
     Q_SLOT void streamOpen();
     Q_SLOT void testStreamError();
 #endif
+
+    // parsing
+    Q_SLOT void testStartTlsPacket_data();
+    Q_SLOT void testStartTlsPacket();
 };
 
 void tst_QXmppStream::initTestCase()
@@ -178,6 +183,60 @@ void tst_QXmppStream::testStreamError()
     }
 }
 #endif
+
+void tst_QXmppStream::testStartTlsPacket_data()
+{
+    QTest::addColumn<QByteArray>("xml");
+    QTest::addColumn<bool>("valid");
+    QTest::addColumn<QXmppStartTlsPacket::Type>("type");
+
+#define ROW(name, xml, valid, type) \
+    QTest::newRow(name)             \
+        << QByteArrayLiteral(xml)   \
+        << valid                    \
+        << type
+
+    ROW("starttls", R"(<starttls xmlns="urn:ietf:params:xml:ns:xmpp-tls"/>)", true, QXmppStartTlsPacket::StartTls);
+    ROW("proceed", R"(<proceed xmlns="urn:ietf:params:xml:ns:xmpp-tls"/>)", true, QXmppStartTlsPacket::Proceed);
+    ROW("failure", R"(<failure xmlns="urn:ietf:params:xml:ns:xmpp-tls"/>)", true, QXmppStartTlsPacket::Failure);
+
+    ROW("invalid-tag", R"(<invalid-tag-name xmlns="urn:ietf:params:xml:ns:xmpp-tls"/>)", false, QXmppStartTlsPacket::StartTls);
+
+#undef ROW
+}
+
+void tst_QXmppStream::testStartTlsPacket()
+{
+    QFETCH(QByteArray, xml);
+    QFETCH(bool, valid);
+    QFETCH(QXmppStartTlsPacket::Type, type);
+
+    QDomDocument doc;
+    QVERIFY(doc.setContent(xml, true));
+    QCOMPARE(QXmppStartTlsPacket::isStartTlsPacket(doc.documentElement()), valid);
+    QCOMPARE(QXmppStartTlsPacket::isStartTlsPacket(doc.documentElement(), type), valid);
+
+    // test other types return false
+    for (auto testValue : { QXmppStartTlsPacket::StartTls,
+                            QXmppStartTlsPacket::Proceed,
+                            QXmppStartTlsPacket::Failure }) {
+        QCOMPARE(QXmppStartTlsPacket::isStartTlsPacket(doc.documentElement(), testValue), testValue == type && valid);
+    }
+
+    if (valid) {
+        QXmppStartTlsPacket packet;
+        parsePacket(packet, xml);
+        QCOMPARE(packet.type(), type);
+        serializePacket(packet, xml);
+
+        QXmppStartTlsPacket packet2(type);
+        serializePacket(packet2, xml);
+
+        QXmppStartTlsPacket packet3;
+        packet3.setType(type);
+        serializePacket(packet2, xml);
+    }
+}
 
 QTEST_MAIN(tst_QXmppStream)
 #include "tst_qxmppstream.moc"
