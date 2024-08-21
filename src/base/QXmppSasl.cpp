8e33612ee54a11737ad7c390cbc9460603862344
Linus Jahn <lnj@kaidan.im>
2024-08-21 19:28:49 +0200

    Fix clang build: Add missing deduction guide for overloaded struct

--- src/base/QXmppSasl.cpp
+++ src/base/QXmppSasl.cpp
@@ -28,6 +28,9 @@ template<class... Ts>
 struct overloaded : Ts... {
     using Ts::operator()...;
 };
+// explicit deduction guide
+template<class... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
 
 static QByteArray forcedNonce;
 
